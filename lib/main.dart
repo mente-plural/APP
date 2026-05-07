f288637e484c5df078c8e2bee7672ff15b83b068
Lucas Magalhães <conceicaolucas68@gmail.com>
2026-05-06 22:37:07 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:app/ui/auth/auth_gate.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
-import 'ui/login/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
